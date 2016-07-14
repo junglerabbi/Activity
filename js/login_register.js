3f9f934ad24dbab189ea5b2fa18937271bb8704f
Jay-zombie <353361342@qq.com>
2016-07-14 22:32:41 +0800

    '学生页面基本完成'

--- js/login_register.js
+++
@@ -1,9 +0,0 @@
-var goto_register = function(){
-	$("#login_input").css("display","none");
-	$("#register_input").css("display","block");
-}
-var goto_login = function(){
-	$("#login_input").css("display","block");
-	$("#register_input").css("display","none");
-}
-
